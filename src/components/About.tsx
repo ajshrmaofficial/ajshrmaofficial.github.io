116dd662f7e813991f16f4a558bc53bd41c3edc1
Ajay Sharma <ajshrmaofficial@gmail.com>
2024-05-25 21:56:29 +0530

    chore: Add logo to About component

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,9 +1,10 @@
 import '../styles/App.css'
+import logo from '../assets/ajay sharma logo.png'
 
 function About(){
     return(
         <div id='about' className='about'>
-            
+            <img src={logo} alt="logo" />
         </div>
     )
 }
