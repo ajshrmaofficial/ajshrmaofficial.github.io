96835295212d77b72ae3d3d0b3f0d7a57cc6f33d
Ajay Sharma <ajshrmaofficial@gmail.com>
2024-06-01 22:27:33 +0530

    refactor: Update NavBar component layout and styling

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,10 +1,11 @@
-import '../styles/App.css'
+import MobileNavMenu from './MobileNavBar';
 
 function NavBar(){
     return(
-        <nav className='navbar'>
-            <h3>Ajay Sharma</h3>
-            <ul className='navlinks'>
+        <nav className='flex flex-row justify-between px-10 py-6 fixed w-full bg-white shadow-navBarShadow'>
+            <h3 className='text-lg font-black'>Ajay Sharma</h3>
+            <MobileNavMenu/>
+            <ul className='lg:flex hidden flex-row gap-3 right-0'>
                 <li><a href="#home">Home</a></li>
                 <li><a href="#about">About</a></li>
                 <li><a href="">Projects</a></li>
