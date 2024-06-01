4b42e1ee954a4daaef7e76cc148fac2b643c80c2
Ajay Sharma <ajshrmaofficial@gmail.com>
2024-06-01 22:27:38 +0530

    refactor: Add Page component for main content area

---
+++ src/components/Page.tsx
@@ -0,0 +1,13 @@
+// import About from "./About";
+import Home from "./Home";
+
+function Page(){
+    return(
+        <div className="lg:w-5/6 mx-auto"> 
+        <Home/>
+        {/* <About/> */}
+        </div>
+    )
+}
+
+export default Page;
