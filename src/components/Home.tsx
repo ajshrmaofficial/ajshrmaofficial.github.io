9a227e98dbd8f636f9623fb8e0ac3e5c624909df
Ajay Sharma <ajshrmaofficial@gmail.com>
2024-06-01 22:27:22 +0530

    refactor: Update Home component layout and styling

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,30 +1,30 @@
-import '../styles/App.css'
-import ProfileBlob from './ProfileBlob';
-import Typewriter from 'typewriter-effect';
+import Container from "./Container";
+import ProfileBlob from "./ProfileBlob";
+import Typewriter from "typewriter-effect";
 
-function Home(){
-    return(
-        <div id='home' className='home'>
-            <div className='hero-section'>
-                <ProfileBlob/>
-                <div className='hero-title'>
-                <h1>Hey There 👋</h1>
-                <div>
-                <h2>I'm a</h2>
-                <h2>
-                <Typewriter
-                    options={{
-                    strings: ['Full Stack Developer', 'React Native Developer'],
-                    autoStart: true,
-                    loop: true,
-                    }}
-                />
-                </h2>
-                </div>
-                </div>
-            </div>
+function Home() {
+  return (
+    <Container>
+      <div className="flex justify-center w-1/2">
+          <ProfileBlob />
+      </div>
+      <div className="flex flex-col items-center justify-center w-1/2">
+        <h1>Hey There 👋</h1>   
+        <div className="flex w-full gap-2">
+          <h2 className="">
+            I'm a 
+          </h2>
+          <Typewriter
+            options={{
+              strings: ["Full Stack Developer", "React Native Developer"],
+              autoStart: true,
+              loop: true,
+            }}
+          />
         </div>
-    )
+      </div>
+    </Container>
+  );
 }
 
 export default Home;
