b4c9450171e2d662e02e7b10aa0ed3d37e93c3ea
Ajay Sharma <ajshrmaofficial@gmail.com>
2024-06-10 23:14:33 +0530

    fixed home screen message styling

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,6 +1,7 @@
 import Container from "./Container";
 import ProfileBlob from "./ProfileBlob";
 import Typewriter from "typewriter-effect";
+import '../styles/App.css'
 
 function Home() {
   return (
@@ -9,9 +10,9 @@ function Home() {
           <ProfileBlob />
       </div>
       <div className="flex flex-col items-center justify-center w-1/2">
-        <h1>Hey There 👋</h1>   
-        <div className="flex w-full gap-2">
-          <h2 className="">
+        <h1 className="text-3xl font-bold">Hey There 👋</h1>   
+        <div className="flex w-full gap-2 justify-center">
+          <h2 className="text-2xl font-bold">
             I'm a 
           </h2>
           <Typewriter
@@ -19,6 +20,7 @@ function Home() {
               strings: ["Full Stack Developer", "React Native Developer"],
               autoStart: true,
               loop: true,
+              wrapperClassName: "typewriter",
             }}
           />
         </div>
