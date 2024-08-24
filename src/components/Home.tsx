92dcaaed704f660116b1f720ad159e038add4383
Ajay Sharma <ajshrmaofficial@gmail.com>
2024-08-24 21:24:38 +0530

    refactor: removed old typewriter component code

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,29 +1,15 @@
-import Container from "./Container";
+import {Container} from "./Container";
 import ProfileBlob from "./ProfileBlob";
-import Typewriter from "typewriter-effect";
-import '../styles/App.css'
 
 function Home() {
   return (
-    <Container>
+    <Container name="home">
       <div className="flex justify-center w-1/2">
           <ProfileBlob />
       </div>
-      <div className="flex flex-col items-center justify-center w-1/2">
-        <h1 className="text-3xl font-bold">Hey There 👋</h1>   
-        <div className="flex w-full gap-2 justify-center">
-          <h2 className="text-2xl font-bold">
-            I'm a 
-          </h2>
-          <Typewriter
-            options={{
-              strings: ["Full Stack Developer", "React Native Developer"],
-              autoStart: true,
-              loop: true,
-              wrapperClassName: "typewriter",
-            }}
-          />
-        </div>
+      <div className="flex flex-col items-center justify-center gap-6 w-1/2">
+        <h1 className="text-6xl font-bold">Hey There 👋</h1>   
+        
       </div>
     </Container>
   );
