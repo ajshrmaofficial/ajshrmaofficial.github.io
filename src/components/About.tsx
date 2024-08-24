30535e4c2a6ce1611003e0b0a4ea1cc510855e82
Ajay Sharma <ajshrmaofficial@gmail.com>
2024-08-24 21:17:44 +0530

    refactor: Updated About page to use Container Component

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,15 +1,15 @@
-import logo from '../assets/ajay sharma logo.png'
+import logo from "../assets/ajay sharma logo.png";
+import { Container } from "./Container";
 
-function About(){
-    return(
-        <div id='about' className='about'>
-            <img src={logo} alt="logo" />
-            <div>
-                <h3>ABOUT ME</h3>
-                
-            </div>
-        </div>
-    )
+function About() {
+  return (
+    <Container name="about">
+      <img src={logo} alt="logo" />
+      <div>
+        <h3>ABOUT ME</h3>
+      </div>
+    </Container>
+  );
 }
 
 export default About;
