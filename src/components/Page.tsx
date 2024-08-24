fd84888371937fb5efe64e576272ea3820eb7831
Ajay Sharma <ajshrmaofficial@gmail.com>
2024-08-24 21:25:05 +0530

    refactor: included About page in layout

--- src/components/Page.tsx
+++ src/components/Page.tsx
@@ -1,11 +1,11 @@
-// import About from "./About";
+import About from "./About";
 import Home from "./Home";
 
 function Page(){
     return(
         <div className="lg:w-5/6 mx-auto"> 
         <Home/>
-        {/* <About/> */}
+        <About/>
         </div>
     )
 }
