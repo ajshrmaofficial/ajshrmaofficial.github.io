641f3078a5ee26ffebaf00a06492713e858d57f6
Ajay Sharma <ajshrmaofficial@gmail.com>
2024-08-24 21:16:28 +0530

    refactor: Update NavBar component to include logo image

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,9 +1,11 @@
 import MobileNavMenu from './MobileNavBar';
+import logoSrc from '../assets/ajay sharma logo.png';
 
 function NavBar(){
     return(
         <nav className='flex flex-row justify-between px-10 py-6 fixed w-full bg-white shadow-navBarShadow'>
-            <h3 className='text-lg font-black'>Ajay Sharma</h3>
+            {/* <h3 className='text-lg font-black'>Ajay Sharma</h3> */}
+            <img src={logoSrc} alt="Ajay Sharma"  className='h-10'/>
             <MobileNavMenu/>
             <ul className='lg:flex hidden flex-row gap-3 right-0'>
                 <li><a href="#home">Home</a></li>
