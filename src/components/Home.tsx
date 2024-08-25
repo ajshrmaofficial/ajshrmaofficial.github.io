b466452aa8852da1a6ad7046bfe93fd4d43c85b3
Ajay Sharma <ajshrmaofficial@gmail.com>
2024-08-25 12:02:55 +0530

    refactor: resume download option added and some layout/styling fixes

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -8,8 +8,9 @@ function Home() {
           <ProfileBlob />
       </div>
       <div className="flex flex-col items-center justify-center gap-6 w-1/2">
-        <h1 className="text-6xl font-bold">Hey There 👋</h1>   
-        
+        <h2 className="lg:text-4xl text-2xl font-bold">Hey There 👋</h2>   
+        <h1 className="text-6xl font-bold">I'm Ajay Sharma</h1>
+        <a className="border-2 border-gray-400 lg:p-3 p-1 font-bold lg:text-lg text-md hover:border-black" href="/files/Ajay Sharma Resume V4.pdf"  target="_blank" rel="noopener noreferrer"><h2>Download Resume</h2></a>
       </div>
     </Container>
   );
