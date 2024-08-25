20cef77441655e37c8bec9078e23034683ee4a1b
Ajay Sharma <ajshrmaofficial@gmail.com>
2024-08-25 12:02:05 +0530

    refactor: new content added to about section and removed old layout

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,14 +1,43 @@
-import logo from "../assets/ajay sharma logo.png";
-import { Container } from "./Container";
-
 function About() {
   return (
-    <Container name="about">
-      <img src={logo} alt="logo" />
-      <div>
-        <h3>ABOUT ME</h3>
+    <div id="about" className="min-h-svh flex flex-col items-center justify-center gap-3 pt-20">
+      <h2 className="lg:text-6xl text-4xl font-bold">ABOUT ME</h2>
+      <div className="flex flex-col items-center text-center gap-3 text-xl p-3">
+        <p>
+        👋 Hey there! I'm <b>Ajay Sharma</b>, a <b>Full Stack Developer</b> based
+          in New Delhi. 🏙️
+        </p>
+        <p>
+          At 22, I'm wrapping up my{" "}
+          <b>B.Tech in Computer Science and Engineering</b><br/> but my passion for
+          coding has been running strong for years. 💻
+        </p>
+        <p>
+          I love working with React ⚛️ Node.js 🟢 and Spring Boot 🍃 to build robust
+          web applications.
+        </p>
+        <p>
+          <b>Mobile development?</b> That's where <b>React Native</b> 📱 comes in
+          handy.
+        </p>
+        <p>
+          I'm also pretty comfortable with libraries like <b>Socket.io</b> and&nbsp;  
+          <b>Redux</b> to add that extra functionality when needed.
+        </p>
+        <p>
+          When I'm not coding, you might find me tinkering with <b>Linux</b> -
+          it's a bit of a hobby.
+        </p>
+        <p>
+          Oh, and Docker 🐳? It's become an essential part of my project workflow.
+        </p>
+        <p>
+          I enjoy the challenge of creating efficient, scalable solutions,
+          whether it's for the web or mobile.<br/> Always eager to learn and grow in
+          this ever-evolving tech world! 🚀
+        </p>
       </div>
-    </Container>
+    </div>
   );
 }
 
