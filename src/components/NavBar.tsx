48ae3b5e829be2552453829902e33ae8376ec517
Ajay Sharma <ajshrmaofficial@gmail.com>
2024-08-25 12:03:53 +0530

    refactor: complete navbar refactored to use map over section array instead of manual lists and mobile navbar code also added here

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,19 +1,35 @@
-import MobileNavMenu from './MobileNavBar';
 import logoSrc from '../assets/ajay sharma logo.png';
+import { useState } from 'react';
+
+const navBarContents = ['Home', 'About', 'Projects', 'Contact'];
 
 function NavBar(){
     return(
-        <nav className='flex flex-row justify-between px-10 py-6 fixed w-full bg-white shadow-navBarShadow'>
+        <nav className='flex flex-row justify-between items-center sm:px-10 sm:py-6 px-2 py-2 fixed w-full bg-white shadow-navBarShadow h-16 sm:h-20'>
             {/* <h3 className='text-lg font-black'>Ajay Sharma</h3> */}
             <img src={logoSrc} alt="Ajay Sharma"  className='h-10'/>
             <MobileNavMenu/>
             <ul className='lg:flex hidden flex-row gap-3 right-0'>
-                <li><a href="#home">Home</a></li>
-                <li><a href="#about">About</a></li>
-                <li><a href="">Projects</a></li>
-                <li><a href="">Contact</a></li>
+                {navBarContents.map(item=>(
+                    <li className='hover:font-bold'><a href={`#${item.toLowerCase()}`}>{item}</a></li>
+                ))}
             </ul>
         </nav>
+    )
+}
+
+function MobileNavMenu(){
+    const [isOpen, setIsOpen] = useState(false);
+    return(
+        <div className="lg:hidden cursor-pointer absolute right-10">
+            <h3 className="font-medium" onClick={()=>{setIsOpen(!isOpen)}}>{isOpen ? 'Close' : 'Menu'}</h3>
+            {isOpen && <ul className='flex flex-col items-end gap-3'>
+                {navBarContents.map(item=>(
+                    <li><a href={`#${item.toLowerCase()}`}>{item}</a></li>
+                ))}
+            </ul> 
+            }
+        </div>
     )
 }
 
