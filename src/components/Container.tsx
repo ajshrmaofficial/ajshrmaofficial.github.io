f1f12ae54aa3100dfe8b6c501fdb6393620f64aa
Ajay Sharma <ajshrmaofficial@gmail.com>
2024-08-27 22:20:13 +0530

    made wrap property of container optional

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,4 +1,4 @@
-export function Container({children, name, wrap}: { children?: React.ReactNode; name: string, wrap: boolean }){
+export function Container({children, name, wrap}: { children?: React.ReactNode; name: string, wrap?: boolean }){
     return(
         <div id={name} className={`flex flex-col min-h-svh items-center gap-10 justify-center lg:justify-between lg:flex-row ${wrap ? 'flex-wrap' : ''}`}>
             {children}
