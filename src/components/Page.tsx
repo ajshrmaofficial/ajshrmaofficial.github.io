5e663a287000924ccfa34f6d9be44bc9af99597e
Ajay Sharma <ajshrmaofficial@gmail.com>
2024-08-27 22:11:51 +0530

    added Projects page to portfolio

--- src/components/Page.tsx
+++ src/components/Page.tsx
@@ -1,13 +1,15 @@
 import About from "./About";
 import Home from "./Home";
+import Projects from "./Projects";
 
-function Page(){
-    return(
-        <div className="lg:w-5/6 mx-auto"> 
-        <Home/>
-        <About/>
-        </div>
-    )
+function Page() {
+  return (
+    <div className="lg:w-5/6 mx-auto">
+      <Home />
+      <About />
+      <Projects />
+    </div>
+  );
 }
 
 export default Page;
