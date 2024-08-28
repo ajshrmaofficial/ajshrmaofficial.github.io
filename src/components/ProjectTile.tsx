849b11c5a467b0c6c899e5d21063af74b3d92ef2
Ajay Sharma <ajshrmaofficial@gmail.com>
2024-08-28 23:14:15 +0530

    style: added target blank to links and added cursor not allowed wrapper for empty links

--- src/components/ProjectTile.tsx
+++ src/components/ProjectTile.tsx
@@ -41,13 +41,16 @@ function ProjectTile({repoOwner, repository, description, liveLink, repoLink}: P
       <p className="font-bold">{repoOwner}/{repository}</p>
       <p className="text-gray-600">{truncateText(description, 115)}</p>
       <div className="flex justify-between">
-        {/* <TechStack techArray={techArr} /> */}
-        <a href={liveLink} className={`border p-2 lg:w-1/5 rounded-xl bg-[#2b3a52] text-white font-bold hover:shadow-lg ${!liveLink ? 'pointer-events-none' : 'pointer-events-auto'}`}>
-          Visit
-        </a>
-        <a href={repoLink} className="border p-2 lg:w-1/5 rounded-xl bg-[#2b3a52] text-white font-bold hover:shadow-lg">
-          GitHub
-        </a>
+        <span  className={`border p-2 lg:w-1/5 rounded-xl bg-[#2b3a52] text-white font-bold hover:shadow-lg ${!liveLink ? 'cursor-not-allowed' : 'cursor-pointer'}`}>
+          <a target="_blank" href={liveLink} className={`${!liveLink ? 'pointer-events-none' : 'pointer-events-auto'}`}>
+            Visit
+          </a>
+        </span>
+        <span className="border p-2 lg:w-1/5 rounded-xl bg-[#2b3a52] text-white font-bold hover:shadow-lg">
+          <a target="_blank" href={repoLink}>
+            GitHub
+          </a>
+        </span>
       </div>
     </div>
   );
