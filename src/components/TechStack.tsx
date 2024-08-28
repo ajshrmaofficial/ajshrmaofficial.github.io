3eac6acbf603509cab041ec553b58b35aadf0bd3
Ajay Sharma <ajshrmaofficial@gmail.com>
2024-08-28 22:55:55 +0530

    refactor: removed dynamic code from techstack component and made it a static/simple component instead

--- src/components/TechStack.tsx
+++ src/components/TechStack.tsx
@@ -1,37 +1,32 @@
-// import reactIcon from "../assets/svg/react.svg";
-// import reactnativeIcon from "../assets/svg/react-native.svg";
-// import nodejsIcon from "../assets/svg/nodejs.svg";
-// import socketioIcon from "../assets/svg/socket.io.svg";
-// import reactqueryIcon from "../assets/svg/react-query.svg";
-// import springIcon from "../assets/svg/spring.svg";
-// import cIcon from "../assets/svg/c.svg";
-// import linuxIcon from "../assets/svg/linux.svg";
-// import dockerIcon from "../assets/svg/docker.svg";
-// import bashIcon from "../assets/svg/bash.svg";
+import reactIcon from "../assets/svg/react.svg";
+import reactnativeIcon from "../assets/svg/react-native.svg";
+import nodejsIcon from "../assets/svg/nodejs.svg";
+import socketioIcon from "../assets/svg/socket.io.svg";
+import reactqueryIcon from "../assets/svg/react-query.svg";
+import springIcon from "../assets/svg/spring.svg";
+import cIcon from "../assets/svg/c.svg";
+import linuxIcon from "../assets/svg/linux.svg";
+import dockerIcon from "../assets/svg/docker.svg";
+import bashIcon from "../assets/svg/bash.svg";
 
-// const techArray = [
-//   { name: "React", path: reactIcon },
-//   { name: "React Native", path: reactnativeIcon },
-//   { name: "NodeJS", path: nodejsIcon },
-//   { name: "Socket.io", path: socketioIcon },
-//   { name: "React Query", path: reactqueryIcon },
-//   { name: "Spring Boot", path: springIcon },
-//   { name: "C", path: cIcon },
-//   { name: "Linux", path: linuxIcon },
-//   { name: "Docker", path: dockerIcon },
-//   { name: "Bash", path: bashIcon },
-// ];
+const techArray = [
+  { name: "React", path: reactIcon },
+  { name: "React Native", path: reactnativeIcon },
+  { name: "NodeJS", path: nodejsIcon },
+  { name: "Socket.io", path: socketioIcon },
+  { name: "React Query", path: reactqueryIcon },
+  { name: "Spring Boot", path: springIcon },
+  { name: "C", path: cIcon },
+  { name: "Linux", path: linuxIcon },
+  { name: "Docker", path: dockerIcon },
+  { name: "Bash", path: bashIcon },
+];
 
-type techArrType = {
-    name: string;
-    path: string;
-}
-
-function TechStack({techArray}: {techArray: techArrType[]}): React.JSX.Element {
+function TechStack() {
   return (
-    <div className="flex gap-2">
+    <div className="flex gap-2 lg:w-full justify-center flex-wrap">
       {techArray.map((tech) => (
-        <img src={tech.path} alt={tech.name} key={tech.name} className="w-14 h-14 border p-2 rounded-md cursor-pointer z-50 bg-offWhite"/>
+        <img src={tech.path} alt={tech.name} key={tech.name} className="size-10 lg:size-10 border p-2 rounded-md cursor-pointer bg-offWhite hover:border-[#2b3a52]"/>
       ))}
     </div>
   );
